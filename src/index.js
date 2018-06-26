9ccb6ef7dd43440bc84597aec45ef39e97db4de5
angelvinyals <angel.vinyals@gmail.com>
2018-06-26 13:24:47 +0200

    changing BrowserRouter to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux'
@@ -12,9 +11,7 @@ const store= createStore(reducer, middleware)
 
 ReactDOM.render(
 	<Provider store={store}>
-		<Router>
-	    	<App/>
-	    </Router>
+		<App />	    
 	</Provider>, 
 	document.getElementById('root')
 );
