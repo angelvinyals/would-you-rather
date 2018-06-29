d459bcb81b08542f1f97c98c5f45517d5305e36b
angelvinyals <angel.vinyals@gmail.com>
2018-06-29 12:32:42 +0200

    Added Page 404- not found
    added one componetn NoMatch a 404 message

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,11 +1,38 @@
-import React  from 'react';
+import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
+import { connect } from 'react-redux'
+import './NoMatch.css';
 
-const NoMatch = ({location}) => (
-  <div>
-    <h3>
-      No match for <code>{location.pathname}</code>
-    </h3>
-  </div>
-);
 
-export default NoMatch;
+class NoMatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navigate: false
+        }
+    }
+
+    componentDidMount(e) {
+        return setTimeout(() => this.setState({ navigate: true }), 1500)      
+
+    };
+
+    render() {
+        if (this.state.navigate) {        	      	
+          	return <Redirect to="/" />
+        }
+
+        return (
+          <div className="FourOhFour">
+                <div className="bg" style={{backgroundColor: 'rgba(201, 76, 76, 0.3)'}}></div>
+                <div className="code">404</div>
+                <div>
+                <h3>page not found</h3>
+                <p>We are sorry but the page you are looking for does not exist.</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect()(NoMatch);
